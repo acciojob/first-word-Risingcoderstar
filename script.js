884281a4406ad78ac2adcde7efd37bbe973555af
Risingcoderstar <94625582+Risingcoderstar@users.noreply.github.com>
2025-04-05 14:36:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+let str="";
+for(let i=0;i<s.length;i++)
+{
+if(s[i]==' ')
+{
+return str;
 }
-
-// Do not change the code below
+str+=s[i];
+}
+return str;
+}
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+// Do not change the code below
+
